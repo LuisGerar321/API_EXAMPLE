d1fb3cb6466dd67643626a4504776ea3e02fd712
AdrianGongora <adrianf8000@hotmail.com>
2021-12-04 01:55:29 -0600

    feat/02 Added feature both router endpoint and logic

--- app/controllers/raspberryActions.js
+++ app/controllers/raspberryActions.js
@@ -14,6 +14,16 @@ class RaspberryPi {
 	ledOff(){
 		rpio.write(this.led, rpio.LOW)
 	};
+
+	turn_OnLedNtimes(n){
+		for (let i = 1; i<= n; i++){ // We execute a for-loop with 'n' as a limit
+			this.ledOn(); // Turn the led on
+			rpio.msleep(1000); // Wait 1000ms
+			this.ledOff(); // Turn the led Off
+			rpio.msleep(1000); // Wait 1000ms
+		}; // Repeat until i=5
+	}
+
 	get	pinLed() {
 		return this.led;
 	};
